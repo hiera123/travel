d0e6c82e396e245f66ef8a4b6fe1ad21f997b68a
小七 <@saxc@sina.com>
2020-02-28 23:14:44 +0800

    城市页面布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,49 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list :hotCities="hotCities" :cities="cities"></city-list>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import axios from 'axios'
 export default {
     name:'City',
+    data () {
+        return {
+        hotCities:[],
+        cities:{}
+
+        }
+  },
     components:{
         CityHeader,
-        CitySearch
-    }
+        CitySearch,
+        CityList
+    },
+    methods:{
+        getCityInfo () {
+        axios.get('/api/city.json').then(this.getCityInfoSucc)
+        },
+        getCityInfoSucc (res) {
+            
+            
+            res = res.data
+            if (res.ret && res.data) {
+                const data = res.data
+                this.hotCities = data.hotCities
+                this.cities = data.cities
+            
+            }
+        
+        }
+  },
+    mounted () {
+    this.getCityInfo()
+  }
 }
 </script>
 
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+
+        <div class="area"> 
+            <div class="title border-topbottom">您的位置</div> 
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div> 
+                </div>
+            </div>
+        </div>
+
+         <div class="area"> 
+            <div class="title border-topbottom">热门城市</div> 
+             <div class="button-list" >
+                <div class="button-wrapper" v-for="item of hotCities" :key="item.id">
+                    <div class="button">{{item.name}}</div> 
+                </div>
+            </div>
+        </div>
+
+        <div class="area"> 
+            <div v-for="(items,key) of cities" :key="items.id">
+              
+                <div class="title border-topbottom">{{key}}</div> 
+                <div v-for="item of items" :key="item.id">
+                    <div class="button button-city border-bottom">{{item.name}}</div> 
+                </div>
+                
+            </div>
+        </div>
+       
+    </div>
+</template>
+
+<script>
+export default {
+   name:'CityList' ,
+   props:{
+       hotCities : Array,
+       cities : Object
+   },
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+    .border-topbottom
+        &:before
+            border-color:#ccc
+        &:after
+            border-color:#ccc
+    .title
+        background :#eee
+        padding:.16rem
+        line-height:.44rem
+        color:#666
+    .button-list
+        overflow :hidden
+        padding:.1rem
+        margin-right :.4rem
+      
+        .button-wrapper
+            width :33%
+            float :left
+            .button
+               text-align :center
+               border :.02rem solid #ccc
+               padding :.16rem
+               margin :.2rem
+               border-radius :.06rem
+    .button-city
+        margin:.2rem 0 .2rem .2rem
+        padding-bottom :.1rem
+       
+   
+        
+           
+        
+          
+   
+</style>
