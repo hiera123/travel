4a0637de70ac74d08147887f19a0f8b8a1a3ded2
小七 <@saxc@sina.com>
2020-02-28 11:33:21 +0800

    首页区域完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <home-header></home-header>
-    <Home-swiper></Home-swiper>
-    <home-icons></home-icons>
-    <home-recommend></home-recommend>
-    <home-weekend></home-weekend>
-    text
+    <Home-swiper :list="swiperList"></Home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
+   
    
   </div>
 </template>
@@ -16,11 +16,16 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
   name: 'Home',
   data () {
     return {
-      
+      swiperList:[],
+      iconList:[],
+      recommendList:[],
+      weekendList:[]
+
     }
   },
   components:{
@@ -29,6 +34,25 @@ export default {
     HomeIcons,
     HomeRecommend,
     HomeWeekend
+  },
+  methods:{
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+      
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- 轮播图 -->
     <div class="wrapper">
-        <swiper :options="swiperOption">
+        <swiper :options="swiperOption" v-if="list.length">
             <!-- slides -->
             <swiper-slide v-for="item of list" :key = "item.id">
                 <img :src="item.imgUrl" class="swiper-img">
@@ -18,6 +18,9 @@
 <script>
 export default {
     name:"HomeSwiper", 
+    props:{
+        list: Array
+    },
     data() {
       return {
         swiperOption: {
@@ -26,20 +29,8 @@ export default {
           // ...
           pagination:'.swiper-pagination',
           loop:true
-        },
-        list:[{
-            "id": "0001",
-            "imgUrl": "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
-            },{
-            "id": "0002",
-            "imgUrl": "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
-            },{
-            "id": "0003",
-            "imgUrl": "http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg"
-            },{
-            "id": "0004",
-            "imgUrl": "http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg"
-        }]
+        }
+       
       }
     }
    
