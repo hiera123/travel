aa590f53f18bf0b8e4a2ab6057d5a32767294d0c
1096225929@qq.com <@saxc@sina.com>
2020-03-08 23:43:10 +0800

    城市列表页面完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
@@ -24,8 +25,8 @@ export default {
       swiperList:[],
       iconList:[],
       recommendList:[],
-      weekendList:[]
-
+      weekendList:[],
+      lastCity:''
     }
   },
   components:{
@@ -35,9 +36,13 @@ export default {
     HomeRecommend,
     HomeWeekend
   },
+  computed:{
+    ...mapState(['city1'])
+  },
   methods:{
     getHomeInfo () {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city1)
+        .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
       res = res.data
@@ -52,7 +57,16 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city1
     this.getHomeInfo()
+  },
+  //页面重新被显示的时候,actived一定会被重新执行
+  //判断当前页面的城市和上次页面城市是否相同,如果不同则重新发送ajax请求
+  activated (){
+    if(this.lastCity !== this.city1){
+        this.lastCity = this.city1
+        this.getHomeInfo()
+    }
   }
 }
 </script>
